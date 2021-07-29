403f1289ab42aa8515b1156b39f4988a707281a7
wanshifu <ninputerwonder@outlook.com>
2021-07-29 16:16:51 +0800

    replace type code with state/ move method

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,11 +1,55 @@
 package org.example;
 
+abstract class Price {
+    abstract int getPriceCode();
+
+    double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+}
+
+class ChildrenPrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
+
+class RegularPrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
+
+class NewReleasePrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -13,11 +57,23 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case CHILDRENS:
+                _price = new ChildrenPrice();
+                break;
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("error arg");
+        }
     }
 
     public String getTitle() {
@@ -25,21 +81,7 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     int getFrequnetRenterPoints(int daysRented) {
