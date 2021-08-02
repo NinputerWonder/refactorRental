65a1dffd5d88a53d3b502a235aa54dd7c7706058
wanshifu <ninputerwonder@outlook.com>
2021-08-02 14:18:22 +0800

    use enum factory to create price

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String _title;
-    private Price _price;
     private PriceCode _priceCode;
 
     public Movie(String title, int priceCode) {
@@ -13,27 +14,8 @@ public class Movie {
        setPriceCode(priceCode);
     }
 
-    public int getPriceCode() {
-        return _priceCode.getCode();
-    }
-
     public void setPriceCode(int arg) {
-        switch (arg) {
-            case CHILDRENS:
-                _price = new ChildrenPrice();
-                _priceCode = PriceCode.CHILDRENDS;
-                break;
-            case REGULAR:
-                _price = new RegularPrice();
-                _priceCode = PriceCode.REGULAR;
-                break;
-            case NEW_RELEASE:
-                _price = new NewReleasePrice();
-                _priceCode = PriceCode.NEW_RELEASE;
-                break;
-            default:
-                throw new IllegalArgumentException("error arg");
-        }
+        _priceCode = Arrays.stream(PriceCode.values()).filter(p -> p.getCode() == arg).findFirst().orElseThrow(() -> new IllegalArgumentException("error arg"));
     }
 
     public String getTitle() {
@@ -41,11 +23,11 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        return _price.getCharge(daysRented);
+        return _priceCode.create().getCharge(daysRented);
     }
 
     int getFrequnetRenterPoints(int daysRented) {
-        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+        if ((_priceCode == PriceCode.NEW_RELEASE) && daysRented > 1)
             return 2;
         return 1;
     }
--- src/main/java/org/example/PriceCode.java
+++ src/main/java/org/example/PriceCode.java
@@ -1,16 +1,26 @@
 package org.example;
 
+import java.util.function.Supplier;
+
 public enum PriceCode {
-    REGULAR(0), NEW_RELEASE(1), CHILDRENDS(2);
+    REGULAR(0, () -> new RegularPrice()),
+    NEW_RELEASE(1, () -> new NewReleasePrice()),
+    CHILDRENDS(2, () -> new ChildrenPrice());
 
     private int code;
+    private Supplier<Price> supplier;
 
-    PriceCode(int code) {
+    PriceCode(int code, Supplier<Price> supplier) {
 
         this.code = code;
+        this.supplier = supplier;
     }
 
     public int getCode() {
         return code;
+    }
+
+    public Price create() {
+        return this.supplier.get();
     }
 }
